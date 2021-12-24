7f83a26731b03926a790b44905242431da161dc3
Arthur Tyukayev <github_arthur@tyukayev.com>
2021-12-23 18:14:02 -0600

    Added typescript declaration files

---
+++ lib/useKeyboardShortcut.d.ts
@@ -0,0 +1,11 @@
+export default useKeyboardShortcut;
+
+type callbackFn = (heldKeys: string[]) => void;
+declare function useKeyboardShortcut(
+  shortcutKeys: string[],
+  callback: callbackFn,
+  options?: {
+    overrideSystem: boolean;
+    ignoreInputFields: boolean;
+  }
+): void;
---
+++ lib/utils.d.ts
@@ -0,0 +1,8 @@
+export function overrideSystemHandling(e: KeyboardEvent): void;
+export function uniq_fast(a: any[]): any[];
+export function checkHeldKeysRecursive(
+  shortcutKey: string,
+  shortcutKeyRecursionIndex: number,
+  shortcutArray: string[],
+  heldKeysArray: string[]
+): boolean;
